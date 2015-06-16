cd3b0fd62f8debf4654cc09f6ad20ff5f58916e6
mammawhy9 <mammawhy9@gmail.com>
2015-06-16 20:01:22 +0200

    poprawka session start

--- include/site.php
+++ include/site.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +11,7 @@
  *
  * @author Piotrek
  */
-session_start();
+
 require 'include/Content.php';
 class Site {
     private $title;
