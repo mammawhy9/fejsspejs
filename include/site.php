5a59fd4de5fca824a7257c438fbb35518b71d5fa
mammawhy9 <mammawhy9@gmail.com>
2015-06-16 18:23:50 +0200

    wstęp do klasy site

--- include/site.php
+++ include/site.php
@@ -13,12 +13,31 @@
  */
 session_start();
 class site {
+    public $title;
+    public $user;
+    
+    public function writeHead(){
+      echo "<html>    <head>        <meta charset='ISO-8859-2'>        <title></title>    </head>  <body>";
+    }
+    
+    public function writeTop(){
+        
+    }
+    
+    public function writeMid(){
+        
+    }
+    public function writeBottom(){
+        echo "   </body> </html>";
+
+    }
+    
     public function setSession(){
         if(!isset($_SESSION['active'])){
             $_SESSION['active']=1;
             
         }
-}
+    }
     public function addSession(){
         if(isset($_SESSION['active'])){
             $_SESSION['active']++;
--- index.php
+++ index.php
@@ -4,25 +4,17 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="ISO-8859-2">
-        <title></title>
-    </head>
-    <body>
         <?php
         require 'include/site.php';
         $site= new site();
+        $site->writeHead()
         if (!isset($_SESSION)) {
     $site->setSession();
     $site->addSession();
     echo $_SESSION['active'];
-    echo "<form action='login.php'>
-                    <button type='submit'/>
-            </form>";
-} else {
-    echo "jest sesja!";
+    echo "<form action='login.php'>                    <button type='submit'/>            </form>";
+    } else {
+        echo "jest sesja!";
 }
 ?>
-    </body>
-</html>
+ 
