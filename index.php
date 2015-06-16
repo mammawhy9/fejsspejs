0091d07c297c4c7c550644936e086ecad1cb2d5d
mammawhy9 <mammawhy9@gmail.com>
2015-06-16 19:20:19 +0200

    Jakieś podstawy są System contentu podstawy Dodanie klasy z kontentem

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-        <?php
-        require 'include/site.php';
-        $site= new site();
-        $site->writeHead()
+<?php
+$tablica= array('head' =>'k','top'=>'l','bottom' => 'o', 'mid' => 'm');
+       require 'include/site.php';
+        $site= new site($tablica);
+        $site->writeHead();
+        $site->writeTop();
+        $site->writeMid();
+        $site->writeBottom();
         if (!isset($_SESSION)) {
-    $site->setSession();
-    $site->addSession();
-    echo $_SESSION['active'];
-    echo "<form action='login.php'>                    <button type='submit'/>            </form>";
-    } else {
+        $site->setSession();
+        $site->addSession();
+        echo $_SESSION['active'];
+        echo "<form action='login.php'>                    <button type='submit'/>            </form>";
+        } else {
         echo "jest sesja!";
 }
 ?>
