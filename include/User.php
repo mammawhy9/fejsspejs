9af89230886ca4d501863aabe22edc8adc905721
mammawhy9 <mammawhy9@gmail.com>
2015-06-17 16:05:14 +0200

    działa odczytywanie tabeli

--- include/User.php
+++ include/User.php
@@ -13,17 +13,30 @@
  */
 class User {
     private $isLogged;
+    private $login;
     //put your code here
-    public function __construct(){
-        if(isset($_SESSION['zalogowany'])){
-            $this->isLogged=$_SESSION['zalogowany'];
+    public function __construct($login='spiety'){
+        $this->login=$login;
+        if(isset($_SESSION['active'])){
+            $this->isLogged=$_SESSION['active'];
         }
     }
     public function logIn(){
       require 'base/connectDB.php';
-        
-            
+      $query="SELECT * FROM users WHERE login = '$this->login';";
+      
+     foreach($userConnection->query("Select * from users;")as $row);{
+         echo $row['login']." ".$row['password'];
+     
+     
+           
     }
     
+    public function setSession(){
+        if(!isset($_SESSION['active'])){
+            $_SESSION['active']=1;
+            
+        }
+    }
     
 }
