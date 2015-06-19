f8548da0a72dcaf047dfaa428bb02f59345243b0
mammawhy9 <mammawhy9@gmail.com>
2015-06-19 11:19:55 +0200

    pierwsze porządki

--- include/site.php
+++ include/site.php
@@ -15,28 +15,28 @@ session_start();
 require 'include/Content.php';
 class Site {
     private $title;
-    public $user;
+    public  $user;
     private $siteContent;
     
     public function __construct($content){
-        $this->siteContent= new Content();
+       $this->siteContent= new Content();
        if(count($content) ==  4){ 
-        $this->siteContent->head = $content['head'];
-        $this->siteContent->top = $content['top'];
-        $this->siteContent->mid = $content['mid'];
-        $this->siteContent->bottom = $content['bottom'];
-        }else{
+            $this->siteContent->head = $content['head'];
+            $this->siteContent->top = $content['top'];
+            $this->siteContent->mid = $content['mid'];
+            $this->siteContent->bottom = $content['bottom'];
+       }else{
             echo "Brak zawarto¶ci  czê¶ci strony!"; exit;
-        }
+       }
     
     }
     
     public function __get($name) {
-        return $this->$name;
+            return $this->$name;
     }
     
     public function writeHead(){
-     echo $this->siteContent->head;
+            echo $this->siteContent->head;
     }
     
     public function writeTop(){
@@ -67,7 +67,7 @@ class Site {
         }
     }
     public function endSession(){
-         session_destroy();
+        session_destroy();
     }
     public function addSession(){
         if(isset($_SESSION['active'])){
--- index.php
+++ index.php
@@ -1,48 +1,49 @@
 <?php
-    
+//       kod testowy, przyk³adowy0    
 $tablica= array('head' =>'<html><head><title></title></head>','top'=>'<div class="top">To jest top</div>','bottom' => '
 </script><div class="bottom">To jest bottom</div>', 'mid' => '<div class="mid">To jest mid</div>');
-       require 'include/site.php';
+// koniec kodu przykladowego
 
+        require 'include/site.php';
+        
         if(!isset($site)){
             $site= new site($tablica);
             $site->setSession();
-            
-            
         } 
-
+        
         $site->writeHead();
         $site->writeTop();
         $site->writeMid();
+        
         echo var_dump($_SESSION);
-        if($_SESSION['logged']==false){
-            require('include/USer.php');
-            $site->user= new User;
-            $site->writeLoginForm();
-            if(isset($_POST['login'])&&isset($_POST['password'])){
-            $site->user->logIn($_POST['login'],$_POST['password']);
-            header("Location: index.php");
-            }
-    
-        }else{
-            echo "zalogowany\n";
-           echo "<form action='index.php'> <input type='hidden' value='true' name='logout'/>                    <button type='submit'>Wyloguj</button>            </form>";
-            
-        }
-        echo (int)$_SESSION['logged'];
-   
-        if($_GET['logout']==true){
-               $site->endSession();
-               header("Location: index.php");
-          }
-        if (!isset($_SESSION)) { 
         
-        
-        echo $_SESSION['active'];
-        echo "<form action='login.php'>                     <button type='submit'/>            </form>";
+        if(!$_SESSION['logged']){
+                require('include/USer.php');
+                $site->user= new User;
+                $site->writeLoginForm();
+                if(isset($_POST['login'])&&isset($_POST['password'])){
+                        $site->user->logIn($_POST['login'],$_POST['password']);
+                        header("Location: index.php");
+                }
         } else {
-        echo "jest sesja!";
-        $site->writeBottom();
-}       
+            echo "zalogowany\n";            
+            // przerobic na metode $site
+            echo "<form action='index.php' method='post'> <input type='hidden' value='true' name='logout'/>                    <button type='submit'>Wyloguj</button>            </form>";
+        }
+        echo $_SESSION['logged'];
+        
+        if(isset($_POST['logout'])){
+            if($_POST['logout']==true){
+                    $site->endSession();
+                     header("Location: index.php");
+            }
+        }
+        if (!isset($_SESSION)) {       
+                echo $_SESSION['active'];
+                // do zamiany na funkcjê
+                echo "<form action='login.php' >                     <button type='submit'/>            </form>";
+        } else {
+            $site->writeBottom();  
+        }       
 
  
