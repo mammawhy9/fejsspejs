df7d5e2bf213873b0aae44e90a016a06d4ed0ea6
mammawhy9 <mammawhy9@gmail.com>
2015-06-20 01:03:36 +0200

    taka tam mała naprawa krytycznego błędu bezpieczeństwa

--- base/connectDB.php
+++ base/connectDB.php
@@ -5,9 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+        require 'baseParam.php';
         try{
-            $userConnection= new PDO("mysql:dbname=fejsspejs;host=localhost",'spiety','kowal');
+            $userConnection= new PDO("mysql:dbname=fejsspejs;host=localhost",$baseLogin,$basePassword);
         } catch (PDOException $e) {
                 echo $e->getMessage();
         }  
